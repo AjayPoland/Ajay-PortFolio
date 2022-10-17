a5af776d5287668353e65a25a1cbbb3c84b97d04
AjayPoland <ajaymanger21@gmail.com>
2022-10-18 01:10:46 +0200

    added one.css/js
    added some js code for counter in js and css for styling and modified index for counter

--- src/component/js-files/one.js
+++ src/component/js-files/one.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+function main() {
+  document.getElementById("cnt01").innerHTML = counter;
+}
+let counter = 0;
+let id=null;
+function startCounter() {
+  id=setInterval(() => {
+    counter++;
+    document.getElementById("cnt01").innerHTML = counter;
+  }, 500);
+  console.log("startcounter");
+}
+function stopCounter(){
+    clearInterval(id);
+    console.log("stopcounter");
+}
