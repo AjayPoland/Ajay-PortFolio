11c474abe9c1ca54b778182fe0354e276c4f0c61
AjayPoland <ajaymanger21@gmail.com>
2022-10-19 00:56:27 +0200

    added setter and getter

--- src/component/js-files/one.js
+++ src/component/js-files/one.js
@@ -3,15 +3,36 @@ function main() {
   document.getElementById("cnt01").innerHTML = counter;
 }
 let counter = 0;
-let id=null;
+let id = null;
 function startCounter() {
-  id=setInterval(() => {
+  id = setInterval(() => {
     counter++;
     document.getElementById("cnt01").innerHTML = counter;
   }, 500);
   console.log("startcounter");
 }
-function stopCounter(){
-    clearInterval(id);
-    console.log("stopcounter");
+function stopCounter() {
+  clearInterval(id);
+  console.log("stopcounter");
+}
+
+let username;
+let obj = {
+  userName: "",
+  set userSet(value) {
+    this.userName = value;
+  },
+  get userGet() {
+    return this.userName;
+  },
+};
+function setUserName() {
+  obj.userSet = "AjayPoland";
+}
+function getUserName() {
+  if (obj.userName=="") {
+    alert("Set the UserName first!");
+  } else {
+    document.getElementById("userName").innerHTML = obj.userGet;
+  }
 }
